327decc0edc6efa42580f490b07137d1c4d26aad
men <1215940173@qq.com>
2018-04-15 03:11:53 +0800

    03点11分

--- information/information-details.js
+++ information/information-details.js
@@ -12,7 +12,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var _this = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        _this.setData({ clientHeight: res.windowHeight })
+      }
+    });
   },
 
   /**
